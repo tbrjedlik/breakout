821c3e0f4bac3b2788666e6238a2c03294da6599
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-02 09:12:20 +0200

    #4 Labda létrehozása

---
+++ js/ball.js
@@ -0,0 +1,10 @@
+class Ball {
+    constructor(x, y, radius, speedX, speedY) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+}
