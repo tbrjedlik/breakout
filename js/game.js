c5fe74f57a7e99a3d4967dfcbfa9d9f42e9658d5
Torma Alex <torma.alex@students.jedlik.eu>
2025-04-02 11:38:31 +0200

    #8 BrickGrid

--- js/game.js
+++ js/game.js
@@ -1,11 +1,13 @@
 import { Ball } from "./ball.js";
 import Paddle from "./paddle.js";
+import BrickGrid from "./brick.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const paddle = new Paddle(50, 10, "#0000FF", canvas);
 const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 4, -4, canvas, paddle);
+const bricks = new BrickGrid(canvas.width)
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -15,6 +17,8 @@ function gameLoop() {
 
     paddle.update();
     paddle.draw(ctx);
+
+    bricks.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
