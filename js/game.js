4e76577fd6978ed7bf36d28bf3221b294dca107f
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-02 10:39:35 +0200

    #25 Main game loop

--- js/game.js
+++ js/game.js
@@ -5,17 +5,6 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 4, -4, canvas);
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ball.move();
-    ball.draw(ctx);
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
 const paddle = new Paddle(50, 10, "#0000FF", canvas);
 
 function drawPaddle() {
@@ -26,13 +15,16 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-function draw() {
+function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawPaddle();
-    paddle.update();
+    ball.move();
+    ball.draw(ctx);
 
-    requestAnimationFrame(draw);
+    paddle.update();
+    drawPaddle();
+
+    requestAnimationFrame(gameLoop);
 }
 
-draw();
+gameLoop();
