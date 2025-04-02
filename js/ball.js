1158046728bb1c2786bd30b0943492f9737714b3
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-02 09:27:23 +0200

    #5 Labda rajzolása és mozgatása

--- js/ball.js
+++ js/ball.js
@@ -1,4 +1,4 @@
-class Ball {
+export class Ball {
     constructor(x, y, radius, speedX, speedY) {
         this.x = x;
         this.y = y;
@@ -7,4 +7,23 @@ class Ball {
         this.speedY = speedY;
     }
 
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    move() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.speedX = -this.speedX;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.speedY = -this.speedY;
+        }
+    }
 }
