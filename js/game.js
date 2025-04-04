ff47cef03cec4eb9245a39bd0f3da34d4db6ed16
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-04 11:05:29 +0200

    #5 Labda iránya random

--- js/game.js
+++ js/game.js
@@ -7,20 +7,33 @@ const ctx = canvas.getContext("2d");
 
 const paddle = new Paddle(50, 10, "#0000FF", canvas);
 const bricks = new BrickGrid(canvas.width);
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 4, -4, canvas, paddle, bricks);
+
+let ballSpeedX = randomFloat(-4, 4);
+// console.log(ballSpeedX)
+const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, ballSpeedX, 4, canvas, paddle, bricks);
+// constructor(x, y, radius, speedX, speedY, canvas, paddle, bricks) {
+
+
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ball.move();
     ball.draw(ctx);
-
+    
     paddle.update();
     paddle.draw(ctx);
-
+    
     bricks.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+function randomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+  gameLoop();
+
+  
