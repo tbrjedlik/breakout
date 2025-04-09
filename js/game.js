da01a138286cbbe526d2c9fa84a5368be90b4b76
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-09 10:45:07 +0200

    #28 életlevonás ha kimegy alul

--- js/game.js
+++ js/game.js
@@ -9,13 +9,13 @@ const paddle = new Paddle(50, 10, "#0000FF", canvas);
 const bricks = new BrickGrid(canvas.width);
 
 let ballSpeedX = randomFloat(-4, 4);
-// console.log(ballSpeedX)
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, ballSpeedX, 4, canvas, paddle, bricks);
-// constructor(x, y, radius, speedX, speedY, canvas, paddle, bricks) {
 
+let ball = new Ball(canvas.width / 2, canvas.height / 2, 10, ballSpeedX, 4, canvas, paddle, bricks);
 
+let lives = 3;
+console.log(lives)
 
-function gameLoop() {
+async function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ball.move();
@@ -27,13 +27,47 @@ function gameLoop() {
     bricks.draw(ctx);
 
     requestAnimationFrame(gameLoop);
+
+    if (ball.y > canvas.height) {
+        lives--;
+        console.log(lives)
+
+        if (lives > 0) {
+
+            ball.speedX = 0;
+            let ballSpeedY = ball.speedY; 
+            ball.speedY = 0;
+            ball.y = canvas.height-1000
+
+            await wait(2000)
+
+
+            ball.x = randomFloat(50,canvas.width - 50);
+            ball.y = canvas.height / 2;
+            ball.speedX = randomFloat(-4, 4);
+            ball.speedY = ballSpeedY
+
+            ball.started = true;
+            
+
+        } else {
+            console.log("Game Over");
+            ball = null;
+            return;
+
+            // TODO: játék végén null hiba
+        }
+    }
 }
 
 function randomFloat(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  gameLoop();
+gameLoop();
 
   
