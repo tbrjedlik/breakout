b0fc9609bb92303db0b39bd76fd1f90525d4bc37
tbrjedlik <toth.barna.robert@students.jedlik.eu>
2025-04-26 19:27:49 +0200

    #28 játék vége null kezelés

--- js/game.js
+++ js/game.js
@@ -12,12 +12,18 @@ let ballSpeedX = randomFloat(-4, 4);
 
 let ball = new Ball(canvas.width / 2, canvas.height / 2, 10, ballSpeedX, 4, canvas, paddle, bricks);
 
-let lives = 3;
+let game = true; 
+let lives = 1;
 console.log(lives)
 
 async function gameLoop() {
+    if (!game){
+        return;
+    }
+    
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    
     ball.move();
     ball.draw(ctx);
     
@@ -25,37 +31,36 @@ async function gameLoop() {
     paddle.draw(ctx);
     
     bricks.draw(ctx);
-
+    
     requestAnimationFrame(gameLoop);
-
+    
     if (ball.y > canvas.height) {
         lives--;
         console.log(lives)
-
+        
         if (lives > 0) {
-
+            
             ball.speedX = 0;
             let ballSpeedY = ball.speedY; 
             ball.speedY = 0;
             ball.y = canvas.height-1000
-
-            await wait(2000)
-
-
+            
+            await wait(2000);
+            
             ball.x = randomFloat(50,canvas.width - 50);
             ball.y = canvas.height / 2;
             ball.speedX = randomFloat(-4, 4);
             ball.speedY = ballSpeedY
-
+            
             ball.started = true;
             
-
+            
         } else {
             console.log("Game Over");
+            await wait(35);
             ball = null;
+            game = false;
             return;
-
-            // TODO: játék végén null hiba
         }
     }
 }
